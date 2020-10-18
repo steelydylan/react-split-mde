fa0d72858eafa6234fc59943ab98704aa0c49f16
steelydylan <ess_president@me.com>
2020-10-18 14:35:00 +0900

    feature(md): add line number to markdown

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,6 +1,8 @@
 import MarkdownIt from "markdown-it";
+import { lineNumber } from "./line-number";
 
 const md = new MarkdownIt();
+lineNumber(md);
 
 export const parser = (value: string) => {
   return md.render(value);
